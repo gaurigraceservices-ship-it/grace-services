1673e542f63323d647d2c759ea7251866ee9051a
Gauri <your-email@gmail.com>
2026-04-14 13:22:14 +0530

    fixed supabase connection

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://plthfzntnlbogtdwboja.supabase.co"
-const supabaseKey = "sb_publishable_U1mdD98TNyS_dRslGGQPqQ_YVGRMNGJ"
+const supabaseUrl = "https://plthfzntnlbogtdwboja.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsdGhmem50aGxib2d0ZHdib2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNzA1MjQsImV4cCI6MjA5MTY0NjUyNH0.x95G6bte9pU3VV23_Eh1GYN7T2tj_njjPuW1-2MdtNY";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
