c2898ce499b09b43b0d1ba255d8cb00716514b5e
Gauri <your-email@gmail.com>
2026-04-14 13:27:00 +0530

    fixed correct supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://plthfzntnlbogtdwboja.supabase.co";
+const supabaseUrl = "https://plthfznthlbogtdwboja.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsdGhmem50aGxib2d0ZHdib2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNzA1MjQsImV4cCI6MjA5MTY0NjUyNH0.x95G6bte9pU3VV23_Eh1GYN7T2tj_njjPuW1-2MdtNY";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
